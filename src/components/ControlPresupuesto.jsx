1b300245fec88ff74eb816a417698701b25dbcad
HalfSeraphic <VIradVT@gmail.com>
2022-07-07 17:30:41 -0500

    swipe

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import NumberFormat from "react-number-format";
+import { useEffect, useState } from "react";
 
-const ControlPresupuesto = ({ presupuesto }) => {
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+
+    const totalDisponible = presupuesto - totalGastado;
+
+    setDisponible(totalDisponible)
+
+    setGastado(totalGastado);
+  }, [gastos]);
+
   const formatearCantidad = (cantidad) => {
     return Number(cantidad).toLocaleString("en-US", {
       style: "currency",
@@ -16,13 +33,16 @@ const ControlPresupuesto = ({ presupuesto }) => {
       </div>
       <div className="contenido-presupuesto">
         <p>
-          <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
+          <span>Presupuesto: </span>
+          {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible: </span>{formatearCantidad(0)}
+          <span>Disponible: </span>
+          {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado: </span>{formatearCantidad(0)}
+          <span>Gastado: </span>
+          {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
