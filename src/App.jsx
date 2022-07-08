b041679e1a88dc64612a3c6c3acdb27a25e1c0e8
HalfSeraphic <VIradVT@gmail.com>
2022-07-07 23:42:09 -0500

    modified:   src/App.jsx modified:   src/components/Gasto.jsx modified:   src/components/ListadoGastos.jsx modified:   src/components/helpers/index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
@@ -9,30 +9,38 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
-  const [modal, setModal] = useState(false)
-  const [animarModal, setAnimarModal] = useState(false)
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
 
-  const [gastos, setGastos] = useState([])
+  const [gastoEditar, setGastoEditar] = useState({})
 
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0 ) {
+      handleNuevoGasto()
+    }
+
+  }, [gastoEditar])
+
+  const [gastos, setGastos] = useState([]);
 
   const handleNuevoGasto = () => {
-    setModal(true)
+    setModal(true);
 
     setTimeout(() => {
-      setAnimarModal(true)
+      setAnimarModal(true);
     }, 500);
-  }
+  };
 
-  const guardarGasto = gasto => {
+  const guardarGasto = (gasto) => {
     gasto.id = generarId();
     gasto.fecha = Date.now();
-    setGastos([...gastos, gasto])
+    setGastos([...gastos, gasto]);
 
     setAnimarModal(false);
     setTimeout(() => {
       setModal(false);
     }, 500);
-  }
+  };
 
   return (
     <div className={modal ? "fijar" : ""}>
@@ -43,21 +51,32 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
-      {isValidPresupuesto &&(
+      {isValidPresupuesto && (
         <>
-        <main>
-          <ListadoGastos gastos={gastos} />
-        </main>
-      <div className="nuevo-gasto">
-      <img
-      src={IconoNuevoGasto}
-      alt="Icono nuevo gasto"
-      onClick={handleNuevoGasto} />
-    </div>
-    </>
+          <main>
+            <ListadoGastos gastos={gastos}
+                  setGastoEditar={setGastoEditar} />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
 
-      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal} guardarGasto={guardarGasto} />}
+      {modal && (
+        <Modal
+        setModal={setModal}
+        animarModal={animarModal}
+        setAnimarModal={setAnimarModal}
+        guardarGasto={guardarGasto}
+        gastoEditar={gastoEditar}
+        setGastoEditar={setGastoEditar}
+        />
+      )}
     </div>
   );
 }
